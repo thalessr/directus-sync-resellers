76b0ea0a611cc2efd301b8367bb9012d0d982081
Thales <thales@perfectline.co>
2025-01-29 16:07:39 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,71 @@
+export default async ({ schedule }, { services, database, getSchema, env, logger }) => {
+  const { ItemsService } = services
+  const schema = await getSchema()
+  const headers = new Headers()
+  const apiUrl = `${env.SHISHI_CATALOG_URL}/api/v1/resellers`
+  const apiUser = env.SHISHI_CATALOG_API_USER
+  const apiPassword = env.SHISHI_CATALOG_API_PASSWORD
+
+  headers.set('Authorization', 'Basic ' + btoa(apiUser + ':' + apiPassword))
+  headers.set('Content-Type', 'application/json')
+
+  const clientsService = new ItemsService('resellers', { schema, database })
+
+  const response = await fetch(apiUrl, { headers: headers, method: 'GET' })
+  const resellers = await response.json()
+
+  // https://crontab.guru/
+  schedule('5 4 * * SUN', async () => {
+    for (const reseller of resellers) {
+      try {
+        const existingClient = await clientsService.readByQuery({
+          filter: { buum_uid: { _eq: reseller.buum_uid } },
+          limit: 1
+        })
+        const jsonData = JSON.parse(reseller)
+        const { slug, ...rest } = jsonData
+
+        if (existingClient.length > 0) {
+          rest.date_updated = new Date().toISOString()
+          logger.info(`Updating client: ${slug}`)
+          await clientsService.updateOne(slug, rest)
+        } else {
+          logger.info(`Creating client: ${slug}`)
+          await clientsService.createOne({ slug, ...rest })
+        }
+      } catch (e) {
+        logger.error(`Error processing reseller: ${reseller}`)
+        logger.error(e)
+      }
+    }
+  })
+
+  schedule('0 2 * * SUN', async () => {
+    const resellersFromDB = await clientsService.readByQuery(
+      {
+        limit: -1,
+        filter: { status: 'published' }
+      }
+    )
+    for (const reseller of resellersFromDB) {
+      try {
+        const response = await fetch(`${apiUrl}/${reseller.buum_uid}`, { headers: headers, method: 'GET' })
+        const apiData = await response.json()
+        if (!apiData) {
+          console.warn(`Archiving: ${reseller.slug}`)
+          reseller.status = 'archived'
+          reseller.date_updated = new Date().toISOString()
+          await clientsService.updateOne(reseller.slug, reseller)
+        }
+      // else{
+      //   const { slug, ...rest } = apiData[0]
+      //   rest.date_updated = new Date().toISOString()
+      //   await clientsService.updateOne(slug)
+      // }
+      } catch (e) {
+        logger.error(`Error processing reseller: ${reseller}`)
+        logger.error(e)
+      }
+    }
+  })
+}
